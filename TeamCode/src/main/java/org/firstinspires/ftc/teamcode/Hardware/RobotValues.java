fd58564086e5e5dc589bf48200e6f3e6aa3e3bbf
Christopher McKenzie <21mckech@students.lovelandschools.org>
2019-11-19 12:45:01 -0500

    Some DUM Dum didntpush code yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotValues.java
@@ -1,26 +1,26 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 
 import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class RobotValues {
+    @SuppressLint("StaticFieldLeak")
     public static Context context;
     public static int WTFAREWEEVENDOING = 0;
-    public static double twentyTicksPerRev = 1120;
-
-    public static double odoDiamMM = 50.8;
-    public static double odoTicksPerRev = 720;
+    static double odoDiamMM = 50.8;
+    static double odoTicksPerRev = 800;
     public static double odoTicksPerRevOddOnesOut = 800;
-    public static double trackWidthmm = 330;
-    public static double middleOdoFromMiddleMM = 163;
+    static double trackWidthmm = 330;
+    static double middleOdoFromMiddleMM = 163;
     public static double heading;
     public static double y;
     public static double x;
-    public static double servo1bottom = .5;
-    public static double servo1top = .3;
-    public static double servo2bottom = .1;
-    public static double servo2top = .2;
-    public static double backHeading = 270;
+    static double servo1bottom = .5;
+    static double servo1top = .3;
+    static double servo2bottom = .1;
+    static double servo2top = .2;
+    static double backHeading = 270;
 }
